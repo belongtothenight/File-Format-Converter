fe0f049adbf12868cc9df54e535a9d8950ce39d3
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2022-07-01 03:05:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import converter_functions as cf
 
+# xml_to_csv(xml_file_path, csv_file_path_name)
 path1 = 'D:/Note_Database/Subject/IITF Industrial Innovation and Technology Foresight/IITF Final Project/IITFFP AI Training/IITFFPAIT Rendered Image/IITFFPAITV Image2/xml'
 path2 = 'D:/Note_Database/Subject/IITF Industrial Innovation and Technology Foresight/IITF Final Project/IITFFP AI Training/IITFFPAIT Rendered Image/IITFFPAITV Image2/train2.csv'
-
 cf.xml_to_csv(path1, path2)
+
